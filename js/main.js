321681fc74c23c6912ed7220c1c0bc67ec2d6bd8
Shadman Jahin <jahin.web@gmail.com>
2025-07-07 12:37:23 +0600

    webpage completed

--- js/main.js
+++ js/main.js
@@ -12,3 +12,54 @@ if (darkModeCheckbox) {
 }
 
 
+
+
+// ===================
+// INITIALIZE SWIPER
+
+const swiper = new Swiper('.carousel-custom', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        // dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // RESPONSIVE
+    watchOverflow: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        }
+    }
+
+
+    // CSS COMMON STYLES
+
+    //  .swiper-pagination-bullet 
+    //  .swiper-pagination-bullet-active
+
+
+
+
+});
+
